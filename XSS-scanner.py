0284e31d4a148852c97fcb92d3b8c27389df36d5
elonapacarizi <74415335+elonapacarizi@users.noreply.github.com>
2022-01-08 00:47:12 +0200

    first update

--- XSS-scanner.py
+++ XSS-scanner.py
@@ -11,3 +11,14 @@ img=Label(window,image=render)
 img.place(x=0,y=0)
 window.title("XSS-SCANNER")
 data=StringVar()
+label1 = Label(window,text="XSS-SCANNER",fg='indigo',bg='powderblue',font=("arial",12,"bold"))
+label1.pack(fill=BOTH,pady=20,padx=20)
+label2 = Label(window,text="Enter the URL you want to scan:",font=("arial",10),fg="indigo",bg="powderblue")
+label2.pack(padx=5,pady=10)
+textbox1=Entry(window,font=("arial",12))
+textbox1.pack()
+def skano():
+    header=""
+    payloads=open("payloads.txt","r")
+    for payload in payloads.readlines():
+        target_with_payload = textbox1.get()+str(payload)
