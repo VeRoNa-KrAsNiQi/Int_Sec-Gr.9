b521dd060a1a038ca46c61603c372c2372b6f315
Diaredaqi1 <75573960+Diaredaqi1@users.noreply.github.com>
2022-01-08 20:32:18 +0100

    Update XSS-scanner.py

--- XSS-scanner.py
+++ XSS-scanner.py
@@ -8,12 +8,14 @@ window=Tk()
 window.geometry("500x500")
 window.minsize(500,500) #madhesia minimale e dritares
 window.maxsize(500,500) #madhesia maksimale e dritares
+
 load=Image.open("images\\a.png")
 render=ImageTk.PhotoImage(load)
 img=Label(window,image=render)
 img.place(x=0,y=0)
 window.title("XSS-SCANNER") #titulli i dritares
 data=StringVar()
+
 #krijimi i label-ve dhe dizajnimi i tyre
 label1 = Label(window,text="XSS-SCANNER",fg='indigo',bg='powderblue',font=("arial",12,"bold"))
 label1.pack(fill=BOTH,pady=20,padx=20)
@@ -23,6 +25,7 @@ textbox1=Entry(window,font=("arial",12))
 textbox1.pack()
 
 
+#Krijimi i funksionit që bënë skanimin e WebSite-ve(URL-ve)
 def scan():
     header=""
     payloads=open("payloads.txt","r")
@@ -35,24 +38,35 @@ testing = requests.get(url=target_with_payload, headers=header)
     else:
         label5=Label(window,text="Webpage is safe!",font=("arial",10),fg="indigo",bg="powderblue")
         label5.pack(padx=2,pady=0)
+        
+        
+#Krijimi i funksionit që mundëson fshirjen e përmbajtjes së textbox-it
 def clearclicked():
     textbox1.delete(0,END)
     textbox1.configure(state="normal")
+    
+    
+#Krijimi i funksionit që mundëson dërgimin e linkut për skanim    
 def submitclicked():
     textbox1.configure(state="normal")
     if validate():
         scan()
+        
+        
+#Funksioni që mundëson validimin e inputit       
 def validate():
      if not ((textbox1.get().startswith("http://") or textbox1.get().startswith("https://"))):
         validateLabel=Label(window,text="URL should start with http:// or https://",fg="indigo",bg="powderblue")
         validateLabel.pack(padx=5,pady=5)
         return False
      return True
+    
+    
+#Krijimi i butonave dhe dizajnimi i tyre    
 button1=Button(window,command=submitclicked,text="Scan",fg='indigo',bg='powderblue',font=("arial",12,"bold"))
 button1.pack(padx=5,pady=10)
 button2=Button(window,command=clearclicked,text="Clear",fg='indigo',bg='powderblue',font=("arial",12,"bold"))
 button2.pack(padx=5,pady=5)
-validateLabel = Label(window, text="", font=("arial",12))
-validateLabel.pack(padx=80, pady=5)
+
 
 window.mainloop()
